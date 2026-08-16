fa9164988ed393482609c96d82bff9299d9142f5
krayem007 <akrammorhag@gmail.com>
2026-08-16 15:08:04 +0100

    connect to Aiven MySQL via DB_PORT + validate SSL with ca.pem

--- server/db.js
+++ server/db.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config({ path: '.env' });
+const useCA = process.env.DB_SSL_CA && fs.existsSync(process.env.DB_SSL_CA);
 module.exports = mysql.createPool({
   host: process.env.db_host,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.db_user,
   password: process.env.db_password,
   database: process.env.db,
@@ -10,7 +13,7 @@ module.exports = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: useCA
+    ? { ca: fs.readFileSync(process.env.DB_SSL_CA), rejectUnauthorized: true }
+    : { rejectUnauthorized: false },
 });
