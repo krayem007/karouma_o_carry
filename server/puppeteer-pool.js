a73f7ba79bab8fab6d54c9ee6aabd565045e3430
krayem007 <akrammorhag@gmail.com>
2026-08-23 15:28:47 +0100

    Revert "fix: align PDF rendering between local and Render"
    This reverts commit 57358feb08bd882fcafeedf2cb6c1673c71755d5.

--- server/puppeteer-pool.js
+++ server/puppeteer-pool.js
@@ -1,25 +1,9 @@
 const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 const fs = require("fs");
 
-const CHROME_ARGS = [
-  "--no-sandbox",
-  "--disable-setuid-sandbox",
-  "--lang=ar",
-  "--disable-features=IsolateOrigins,site-per-process",
-];
-
-const CHROME_PATHS = [
-  "/usr/bin/google-chrome",
-  "/usr/bin/chromium-browser",
-  "/usr/bin/chromium",
-];
-
-function findChromePath() {
-  for (const p of CHROME_PATHS) {
-    if (fs.existsSync(p)) return p;
-  }
-  return null;
-}
+const SYSTEM_CHROME = "/usr/bin/google-chrome";
+const useSystemChrome = fs.existsSync(SYSTEM_CHROME);
 
 class PuppeteerPool {
   constructor(maxBrowsers) {
@@ -55,26 +39,14 @@ class PuppeteerPool {
   }
 
   async _launchBrowser(index) {
-    const systemChrome = findChromePath();
-    let executablePath;
-    let useBundledChromium = false;
-
-    if (systemChrome) {
-      executablePath = systemChrome;
-      console.log(`[POOL] Using system Chrome: ${systemChrome}`);
-    } else {
-      const chromium = require("@sparticuz/chromium");
-      executablePath = await chromium.executablePath();
-      useBundledChromium = true;
-      console.log(`[POOL] Using @sparticuz/chromium: ${executablePath}`);
-    }
-
     const browser = await puppeteer.launch({
-      executablePath,
-      headless: true,
-      args: useBundledChromium
-        ? [...CHROME_ARGS, "--disable-gpu", "--disable-dev-shm-usage"]
-        : CHROME_ARGS,
+      executablePath: useSystemChrome
+        ? SYSTEM_CHROME
+        : await chromium.executablePath(),
+      headless: useSystemChrome ? true : "shell",
+      args: useSystemChrome
+        ? ["--no-sandbox", "--disable-setuid-sandbox", "--lang=ar"]
+        : [...chromium.args, "--lang=ar"],
     });
 
     const entry = { browser, index, alive: true };
